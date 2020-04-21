d28174d2ab072961d4b8e14e89127d4f5e8ccb15
David Begin <davidmichaelbe@gmail.com>
2020-04-21 09:38:20 -0700

    Small update to the revolution

--- chat_thief/revolution.py
+++ chat_thief/revolution.py
@@ -9,15 +9,19 @@ class Revolution:
         self.cool_points = int(self.user.cool_points())
 
     def incite(self):
-        if self.cool_points > 99 or self.user_name == "beginbotbot":
-            results = []
-            result = self.user.purge()
-            results.append(result)
-            permissions_manager = CommandPermissionCenter("beginbot")
-            result = permissions_manager.purge()
-            results.append(result)
-            return results
-
+        if self.user_name == "beginbot" or self.cool_points > 99:
+            self._revolution()
+        elif self.user.total_cool_points() > 60:
+            return "COMING SOON A NEW JOINT REVOLUTION"
             # We want to remove all permissions and points
         else:
             return f"@{self.user_name} you need {99 - self.cool_points} more for a revolution"
+
+    def _revolution(self):
+        results = []
+        result = self.user.purge()
+        results.append(result)
+        permissions_manager = CommandPermissionCenter("beginbot")
+        result = permissions_manager.purge()
+        results.append(result)
+        return results
