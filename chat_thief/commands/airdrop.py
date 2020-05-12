55b2579851e03357f0f842a1611f244e8d862e23
David Begin <davidmichaelbe@gmail.com>
2020-05-12 08:31:35 -0700

    Fixes the Dropping of commands for users
        and adds an airdrop station

--- chat_thief/commands/airdrop.py
+++ chat_thief/commands/airdrop.py
@@ -24,25 +24,6 @@ def drop_effect(user, soundeffect):
         return f"@{user} now has access to Sound Effect: !{soundeffect}"
 
 
-def dropreward():
-    with Path(__file__).parent.parent.joinpath("logs/chat.log") as log:
-        chat_lines = [
-            line
-            for line in log.read_text().split("\n")
-            if line
-            and line.split(":")[0] not in INVALID_USERS
-            and line != CONNECTING_MSG
-        ]
-    user = chat_lines[-1].split(":")[0]
-    soundeffect = random_soundeffect()
-    return drop_effect(user, soundeffect)
-
-
-def drop_random_soundeffect_to_user(user):
-    soundeffect = random_soundeffect()
-    return drop_effect(user, soundeffect)
-
-
 class Airdrop:
     def drop_random_soundeffect_to_random_user(self):
         user = self.random_user()
@@ -50,17 +31,16 @@ class Airdrop:
         return drop_effect(user, soundeffect)
 
     def random_user(self, blacklisted_users=[]):
-        # try:
-        looking_for_user = True
-        while looking_for_user:
-            users = ChatLogs().recent_stream_peasants()
-            user = random.sample(users, 1)[0]
-            if user not in INVALID_USERS + blacklisted_users:
-                looking_for_user = False
-        return user
-        # except:
-        #     traceback.print_exc()
-        #     breakpoint()
+        try:
+            looking_for_user = True
+            while looking_for_user:
+                users = ChatLogs().recent_stream_peasants()
+                user = random.sample(users, 1)[0]
+                if user not in INVALID_USERS + blacklisted_users:
+                    looking_for_user = False
+            return user
+        except:
+            traceback.print_exc()
 
     def __init__(self, target_user=None, target_command=None, amount=None):
         self._target_user = target_user
@@ -68,25 +48,27 @@ class Airdrop:
         self._amount = amount
 
     def drop(self):
-        return self.drop_random_soundeffect_to_random_user()
+        if self._amount:
+            results = []
+            for i in range(0, self._amount):
+                results.append(self._drop())
+            return results
+        return self._drop()
 
-        # if self._target_user is None and self._target_command is None and
-        # self._amount is None
+    def _drop(self):
+        if self._target_user and self._target_command:
+            return drop_effect(self._target_user, self._target_command)
+        elif self._target_user:
+            return self.drop_random_soundeffect_to_user(self._target_user)
+        elif self._target_command:
+            return self.drop_soundeffect_for_random_user(self._target_command)
+        else:
+            return self.drop_random_soundeffect_to_random_user()
 
-    # if len(args) == 0:
-    #     print(f"Dropping Sound Effect since we got no args")
-    #     return drop_random_soundeffect_to_random_user()
-    # else:
-    #     if _is_int_between(args[0]):
-    #         for i in range(0, int(args[0])):
-    #             print(f"Dropping {i} Sound Effect")
-    #             send_twitch_msg(drop_random_soundeffect_to_random_user())
-    #     elif args[0] in WelcomeCommittee().present_users():
-    #         user = args[0]
-    #         print(f"Dropping a Sound Effect for: @{user}")
-    #         return drop_random_soundeffect_to_user(user)
-    #     elif args[0] in SoundeffectsLibrary.fetch_soundeffect_names():
-    #         user = random_user()
-    #         soundeffect = args[0]
-    #         print(f"Dropping the Sound Effect: {soundeffect}")
-    #         return drop_effect(user, soundeffect)
+    def drop_soundeffect_for_random_user(self, soundeffect):
+        user = self.random_user()
+        return drop_effect(user, soundeffect)
+
+    def drop_random_soundeffect_to_user(self, user):
+        soundeffect = random_soundeffect()
+        return drop_effect(user, soundeffect)
--- tests/commands/test_airdrop.py
+++ tests/commands/test_airdrop.py
@@ -9,29 +9,75 @@ from chat_thief.soundeffects_library import SoundeffectsLibrary
 
 class TestAirDrop(DatabaseConfig):
     @pytest.fixture
-    def mock_stream_peasants(self, monkeypatch):
+    def mock_users(self, monkeypatch):
         def _mock_recent_stream_peasants(self):
             return ["thugga"]
-
-        def _mock_soundeffects_only():
-            return ["clap"]
 
         monkeypatch.setattr(
             ChatLogs, "recent_stream_peasants", _mock_recent_stream_peasants
         )
+
+    @pytest.fixture
+    def mock_soundeffects(self, monkeypatch):
+        def _mock_soundeffects_only():
+            return ["clap"]
+
         monkeypatch.setattr(
             SoundeffectsLibrary, "soundeffects_only", _mock_soundeffects_only
         )
 
-    def test_with_no_args(self, mock_stream_peasants):
+    def test_with_no_args(self, mock_users, mock_soundeffects):
         subject = Airdrop()
         user = User("grant.green")
         assert subject.drop() == "@thugga now has access to Sound Effect: !clap"
 
-    # no args
-    # target_user, target_command
-    # target_command
-    # target_user
-    # amount
-    # target_user amount
-    # target_command amount
+    def test_with_target_user(self, mock_soundeffects):
+        subject = Airdrop(target_user="future")
+        user = User("grant.green")
+        assert subject.drop() == "@future now has access to Sound Effect: !clap"
+
+    def test_with_target_command(self, mock_users):
+        subject = Airdrop(target_command="wassup")
+        user = User("grant.green")
+        assert subject.drop() == "@thugga now has access to Sound Effect: !wassup"
+
+    def test_with_user_and_command(self):
+        subject = Airdrop(target_user="wes", target_command="wassup")
+        user = User("grant.green")
+        assert subject.drop() == "@wes now has access to Sound Effect: !wassup"
+
+    def test_with_just_amount(self, mock_users, mock_soundeffects):
+        subject = Airdrop(amount=2)
+        user = User("grant.green")
+        result = subject.drop()
+        assert result == [
+            "@thugga now has access to Sound Effect: !clap",
+            "@thugga now has access to Sound Effect: !clap",
+        ]
+
+    def test_with_amount_and_user(self, mock_soundeffects):
+        subject = Airdrop(target_user="ella", amount=2)
+        user = User("grant.green")
+        result = subject.drop()
+        assert result == [
+            "@ella now has access to Sound Effect: !clap",
+            "@ella now has access to Sound Effect: !clap",
+        ]
+
+    def test_with_amount_and_command(self, mock_users):
+        subject = Airdrop(target_command="handbag", amount=2)
+        user = User("grant.green")
+        result = subject.drop()
+        assert result == [
+            "@thugga now has access to Sound Effect: !handbag",
+            "@thugga now has access to Sound Effect: !handbag",
+        ]
+
+    def test_with_amount_command_and_user(self):
+        subject = Airdrop(target_user="ella", target_command="handbag", amount=2)
+        user = User("grant.green")
+        result = subject.drop()
+        assert result == [
+            "@ella now has access to Sound Effect: !handbag",
+            "@ella now has access to Sound Effect: !handbag",
+        ]
