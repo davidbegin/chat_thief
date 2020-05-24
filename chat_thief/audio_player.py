03089a5460595be33472790ddae9527efd9e31e0
David Begin <davidmichaelbe@gmail.com>
2020-05-24 16:42:25 -0700

    Notification for sounds playing

--- chat_thief/audio_player.py
+++ chat_thief/audio_player.py
@@ -1,7 +1,10 @@
+from pathlib import Path
+
 import subprocess
 import traceback
 
 from chat_thief.config.log import success
+from chat_thief.models.notification import Notification
 
 MPLAYER_VOL_NORM = "0.50"
 
@@ -9,7 +12,10 @@ MPLAYER_VOL_NORM = "0.50"
 class AudioPlayer:
     @staticmethod
     def play_sample(sound_file):
-        success(f"Playing: {sound_file}")
+        sound_name = sound_file.name[: -len(sound_file.suffix)]
+        success(f"Playing: {sound_name}")
+        Notification(f"Playing: {sound_name}").save()
+
         try:
             subprocess.call(
                 ["mplayer", "-af", f"volnorm=2:{MPLAYER_VOL_NORM}", sound_file],
