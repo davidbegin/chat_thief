08b8aa5188cf692ad9c00ca3a5dbf39ba4b48f87
David Begin <davidmichaelbe@gmail.com>
2020-05-25 10:11:28 -0700

    Make the Forbes Bot only report after a Cool Down Period

--- forbes_bot.py
+++ forbes_bot.py
@@ -6,64 +6,93 @@ import time
 from datetime import datetime
 import os
 
-from chat_thief.chat_logs import ChatLogs
-from chat_thief.config.log import logger
-from chat_thief.irc import send_twitch_msg
 from chat_thief.models.breaking_news import BreakingNews
-from chat_thief.models.soundeffect_request import SoundeffectRequest
 from chat_thief.models.user import User
 from chat_thief.models.command import Command
-from chat_thief.prize_dropper import drop_random_soundeffect_to_user
-
-BLACKLIST = []
 
 
-def sync_main():
-    in_coup = False
-    last_most_expensive = Command.most_expensive()
-    last_cool_points = User.richest_cool_points()
+HOW_MANY_SECONDS_BETWEEN_NEWS = 300
 
-    while True:
-        os.system("clear")
 
-        print(f"\nCommand: !{last_most_expensive['name']}")
-        print(
-            f"Cool Points: @{last_cool_points['name']} - {last_cool_points['cool_points']}"
-        )
+class ForbesBot:
+    def __init__(self):
+        self.last_breaking_time = None
+        self.last_most_expensive = Command.most_expensive()
+        self.last_cool_points = User.richest_cool_points()
 
-        # print(f"in coup: {in_coup}")
+    def loop(self):
+        while True:
+            os.system("clear")
 
-        try:
-            new_top_cmd = Command.most_expensive()
-            updated_cool_points = User.richest_cool_points()
+            print(f"\nCommand: !{self.last_most_expensive['name']}")
+            print(
+                f"Cool Points: @{self.last_cool_points['name']} - {self.last_cool_points['cool_points']}"
+            )
 
-            print(f"New Most Expensive: {new_top_cmd['name']}")
-            print(f"Old Most Expensive: {last_most_expensive['name']}")
+            if self.last_breaking_time:
+                how_long_since_break = datetime.now() - self.last_breaking_time
+                print(
+                    f"How Long: {how_long_since_break} / {HOW_MANY_SECONDS_BETWEEN_NEWS}"
+                )
 
-            if new_top_cmd["name"] != last_most_expensive["name"]:
-                print(f"We have a new most expensive command: new_top_cmd['name']")
-                last_most_expensive = new_top_cmd
-                BreakingNews(
-                    f"New Most Expensive Command: {new_top_cmd['name']} - 💸 {new_top_cmd['cost']}"
-                ).save()
+            try:
+                self.new_top_cmd = Command.most_expensive()
+                self.updated_cool_points = User.richest_cool_points()
 
-            elif updated_cool_points["name"] != last_cool_points["name"]:
-                print(f"We have a new richest user: {updated_cool_points['name']}")
-                last_cool_points = updated_cool_points
-                BreakingNews(
-                    scope=f"New Richest in Cool Points: {updated_cool_points['name']}",
-                    user=updated_cool_points["name"],
-                ).save()
+                print(f"New Most Expensive: {self.new_top_cmd['name']}")
+                print(f"Old Most Expensive: {self.last_most_expensive['name']}")
 
-            time.sleep(1)
-        except Exception as e:
-            time.sleep(30)
+                if self.new_top_cmd["name"] != self.last_most_expensive["name"]:
+                    print(
+                        f"We have a new most expensive command: self.new_top_cmd['name']"
+                    )
+                    self.last_most_expensive = self.new_top_cmd
 
-            if e is KeyboardInterrupt:
-                raise e
-            else:
+                    if self.last_breaking_time:
+                        how_long_since_break = datetime.now() - self.last_breaking_time
+                        print(f"How Long: {how_long_since_break}")
+
+                        if how_long_since_break.seconds > HOW_MANY_SECONDS_BETWEEN_NEWS:
+                            self.breaking_news(
+                                f"New Most Expensive Command: {self.new_top_cmd['name']} - 💸{ self.new_top_cmd['cost']}"
+                            ).save()
+                    else:
+                        self.breaking_news(
+                            msg=f"New Most Expensive Command: {self.new_top_cmd['name']} - 💸{ self.new_top_cmd['cost']}"
+                        )
+
+                elif self.updated_cool_points["name"] != self.last_cool_points["name"]:
+                    print(
+                        f"We have a new richest user: {self.updated_cool_points['name']}"
+                    )
+                    self.last_cool_points = self.updated_cool_points
+                    if self.last_breaking_time:
+                        how_long_since_break = datetime.now() - self.last_breaking_time
+                        print(f"How Long: {how_long_since_break}")
+
+                        if how_long_since_break.seconds > 300:
+                            self.breaking_news(
+                                msg=f"New Richest in Cool Points: {self.updated_cool_points['name']}",
+                                user=self.updated_cool_points["name"],
+                            )
+                    else:
+                        self.breaking_news(
+                            msg=f"New Richest in Cool Points: {self.updated_cool_points['name']}",
+                            user=self.updated_cool_points["name"],
+                        )
+
+                time.sleep(1)
+            except Exception as e:
                 traceback.print_exc()
+                time.sleep(1)
+
+                if e is KeyboardInterrupt:
+                    raise e
+
+    def breaking_news(self, msg, user=None):
+        self.last_breaking_time = datetime.now()
+        BreakingNews(scope=msg, user=user,).save()
 
 
 if __name__ == "__main__":
-    sync_main()
+    ForbesBot().loop()
