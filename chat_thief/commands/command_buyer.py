222d012d178de45ccee2d08df1ef6823d2241588
David Begin <davidmichaelbe@gmail.com>
2020-05-27 07:18:02 -0700

    Delete vestigial comments

--- chat_thief/commands/command_buyer.py
+++ chat_thief/commands/command_buyer.py
@@ -7,26 +7,9 @@ class CommandBuyer:
         self.user = User(user)
         self.target_sfx = target_sfx
 
-    # This command relied on the output of buy
-    # And we need to control the output in this class
-    # def buy(self):
-    #     sfxs = []
-    #     for _ in range(0, self.amount):
-    #         if self.target_sfx == "random":
-    #             target_sfx = self.user._find_affordable_random_command().name
-    #         else:
-    #             target_sfx = self.target_sfx
-    #         sfxs.append("!" + target_sfx)
-    #         print(f"{self.user.name} buying {target_sfx}")
-    #         self.user.buy(target_sfx)
-    #     return f"@{self.user.name} bought {len(sfxs)} SFXs: {' '.join(sfxs)}"
-
-    # When you try and purchase random
-    # and fail, we shuld try again
     def new_buy(self):
         results = []
 
-        # what if you don't have enough
         for _ in range(0, self.amount):
             result = self._try_and_buy()
             print(result)
@@ -38,7 +21,6 @@ class CommandBuyer:
         total_spent = sum([result.cost for result in results])
         sfx_names = " ".join(["!" + result.sfx for result in results])
 
-        # if all are successful purchases
         successful_purchase = all(
             [result.result == PurchaseResult.SuccessfulPurchase for result in results]
         )
