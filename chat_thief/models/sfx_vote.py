d1b3cb1866374f7887b0e59f975020a064b237c5
David Begin <davidmichaelbe@gmail.com>
2020-07-07 07:34:28 -0700

    Types on the SFX Vote

--- chat_thief/models/sfx_vote.py
+++ chat_thief/models/sfx_vote.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Dict, Callable, Any
 
 from tinydb import Query  # type: ignore
 
@@ -11,22 +11,25 @@ class SFXVote(BaseDbModel):
     table_name = "sfx_votes"
     database_path = "db/sfx_votes.json"
 
-    def is_enabled(self):
-        if self.supporter_count() == 0 and self.detractor_count() == 0:
-            return True
-
-        return self.supporter_count() >= self.detractor_count()
-
-    def __init__(self, command, supporters=[], detractors=[]):
+    # Incompatible default for argument "detractors" (default has type
+    # "Type[List[Any]]", argument has type "List[Any]")
+    def __init__(self, command: str, supporters: List[Any]=[], detractors: List[Any]=[]):
         self.command = command
         self.supporters = supporters
         self.detractors = detractors
 
-    def support(self, supporter):
+    def doc(self) -> Dict:
+        return {
+            "command": self.command,
+            "supporters": self.supporters,
+            "detractors": self.detractors,
+        }
+
+    def support(self, supporter: str) -> Dict:
         vote = self._find_or_create_vote()
 
-        def show_support(supporter):
-            def transform(doc):
+        def show_support(supporter: str) -> Callable[[Dict], None]:
+            def transform(doc: Dict) -> None:
                 if supporter not in doc["supporters"]:
                     doc["supporters"].append(supporter)
                 if supporter in doc["detractors"]:
@@ -35,14 +38,13 @@ class SFXVote(BaseDbModel):
             return transform
 
         self.db().update(show_support(supporter), Query().command == self.command)
-        # We are returning Dict
         return self._find_or_create_vote()
 
-    def detract(self, detractor):
+    def detract(self, detractor: str) -> Dict:
         vote = self._find_or_create_vote()
 
-        def detract_support(detractor):
-            def transform(doc):
+        def detract_support(detractor: str) -> Callable[[Dict], None]:
+            def transform(doc: Dict) -> None:
                 if detractor not in doc["detractors"]:
                     doc["detractors"].append(detractor)
                 if detractor in doc["supporters"]:
@@ -53,35 +55,28 @@ class SFXVote(BaseDbModel):
         self.db().update(detract_support(detractor), Query().command == self.command)
         return self._find_or_create_vote()
 
-    def like_to_hate_ratio(self):
+    def like_to_hate_ratio(self) -> float:
         if self.detractor_count() < 1:
             return 100
         total = self.supporter_count() + self.detractor_count()
         return (self.supporter_count() / total) * 100
 
-    def supporter_count(self):
+    def supporter_count(self) -> int:
         vote = self._find_or_create_vote()
         return len(vote["supporters"])
 
-    def detractor_count(self):
+    def detractor_count(self) -> int:
         vote = self._find_or_create_vote()
         return len(vote["detractors"])
 
-    def _find_or_create_vote(self):
-        vote = self.db().get(Query().command == self.command)
+    def is_enabled(self) -> bool:
+        if self.supporter_count() == 0 and self.detractor_count() == 0:
+            return True
+        return self.supporter_count() >= self.detractor_count()
 
-        if vote:
+    def _find_or_create_vote(self) -> Dict:
+        if vote := self.db().get(Query().command == self.command):
             return vote
         else:
-            print(f"Creating New SFXVote: {self.doc()}")
-
-            with transaction(self.db()) as tr:
-                tr.insert(self.doc())
+            self.save()
             return self.doc()
-
-    def doc(self):
-        return {
-            "command": self.command,
-            "supporters": self.supporters,
-            "detractors": self.detractors,
-        }
